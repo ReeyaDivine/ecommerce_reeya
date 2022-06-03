3ef4ca73e99db45e44356ebdcab22d476d36a60b
Reeya Agrawal <105407387+ReeyaDivine@users.noreply.github.com>
2022-06-03 07:19:36 -0700

    final

--- product_module/admin.py
+++ product_module/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Brand,Category,Product
+from .models import Brand,Category,Product,ProductAdmin
 
 
 
@@ -9,3 +9,15 @@ admin.site.register(Brand)
 admin.site.register(Category)
 admin.site.register(Product)
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["name","price","brand","category",]
+    search_fields = ["name","price","brand__name","category__name",]
+    list_filter = ["brand", "category",]
+    readonly_fields = ["quantity"]
+
+
+class Meta:
+    model = Product 
+
+
+admin.site.register(ProductAdmin) 
